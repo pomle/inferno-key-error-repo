a94a48f48725954cf96e6188a88b273675a2d7ac
Pontus Persson <pom@skurt.com>
2017-06-10 13:50:25 -0700

    Breaking change.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { version } from 'inferno';
 import Component from 'inferno-component';
+import { List } from 'immutable';
 import './registerServiceWorker';
 import Logo from './logo';
 import './App.css';
 
 class App extends Component {
   render() {
+    const items = new List(['a', 'b', 'c']);
+
     return (
       <div className="App">
         <div className="App-header">
@@ -15,6 +18,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        { items.map(item => <span/>) }
       </div>
     );
   }
